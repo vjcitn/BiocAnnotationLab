c6c7dc19ff91bb18122e3fdc2fd18ccb92d02ba5
vjcitn <stvjc@channing.harvard.edu>
2026-03-01 15:15:14 +0000

    update to handle different positions of taxonomy id!

--- inst/utils/ncbiTax.sh
+++ inst/utils/ncbiTax.sh
@@ -2,13 +2,21 @@
 #
 # ncbiTax: args are taxid, 2 letter tag, NCBI 'stub' gene2[.*].gz
 # uses R program txt2parq.R requiring data.table and arrow
-# example: source ncbiTax.sh 9606 Hs gene2pubmed
+# example: ./ncbiTax.sh 9606 Hs gene2pubmed
 # will produce Hs.gene2pubmed.parquet
 #
 TAXID=$1
 TAG=$2
 STUB=$3
-zcat $STUB.gz | head -1 | gzip - > /tmp/head
-zcat $STUB.gz | grep "^$TAXID	" | gzip - >  /tmp/Hs.$STUB.gz
+echo "$STUB"
+
+zcat "$STUB.gz" | head -1 | gzip - > /tmp/head
+
+if [ "$STUB" = "gene_refseq_uniprotkb_collab" ]; then
+  echo "Using column 3 for taxid filtering (uniprotkb format)"
+  zcat "$STUB.gz" | awk -F'\t' -v taxid="$TAXID" '$3 == taxid' | gzip - > /tmp/Hs.$STUB.gz
+else
+  zcat "$STUB.gz" | awk -F'\t' -v taxid="$TAXID" '$1 == taxid' | gzip - > /tmp/Hs.$STUB.gz
+fi
 cat /tmp/head /tmp/Hs.$STUB.gz > Hs.$STUB.gz
 Rscript -e "source('txt2parq.R'); txt2parq('$STUB')"
