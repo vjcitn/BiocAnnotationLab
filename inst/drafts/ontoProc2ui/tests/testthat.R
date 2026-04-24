91ce4f3627839686ad7e99c9e4168a114e8598e9
vjcitn <stvjc@channing.harvard.edu>
2026-04-24 06:47:51 -0400

    starting a ui for GO

---
+++ inst/drafts/ontoProc2ui/tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(ontoProc2ui)
+
+test_check("ontoProc2ui")
---
+++ inst/preUI/go_ui.R
@@ -0,0 +1,6 @@
+# user interface for GO
+
+#' lookup_curie [GO:nnnnnnn]
+#' @param curies vector of GO:... CURIEs (Compact Uniform Resource IdentifiErs)
+#' @param mapto character, mapping target: "term", "definition", "ontology"
+#' @return a lazy tibble
